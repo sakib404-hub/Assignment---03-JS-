c56833959c9c5b1989f245f292e1e38512b3b7f9
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-08-04 15:56:25 +0600

    solved the 5th problem

--- final.js
+++ final.js
@@ -40,3 +40,14 @@ if (fileName[0] === "#") {
 } else {
   console.log("Delete");
 }
+
+// Problem 05 - PH Email Generator
+var student = {
+  name: "mewo",
+  roll: 96,
+  department: "swe",
+};
+
+var phEmail =
+  student.name + student.roll + "." + student.department + "@ph.ac.bd";
+console.log(phEmail);
