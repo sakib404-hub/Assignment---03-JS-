b34d116278f52aac513bbff388bcc3778b852509
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-08-04 16:14:29 +0600

    added comments

--- final.js
+++ final.js
@@ -9,10 +9,13 @@ console.log(assetEachBrother);
 var money = 45000;
 
 if (money < 10000) {
+  //Logic fo findng if the money is less then 10000 taka
   console.log("Chocolate");
 } else if (money >= 10000 && money < 25000) {
+  //logic for finding if the money is between 10000 to less then 25000 taka
   console.log("Cycle");
 } else if (money >= 25000) {
+  //logic for finding if the money is greater or equal to 25000 taka
   console.log("Laptop");
 }
 
@@ -22,8 +25,10 @@ var lastDay = 6;
 
 for (var day = 1; day <= lastDay; day++) {
   if (day % 3 === 0) {
+    //if the day(iteraor) is divisable by 3 therefore its medicine day
     console.log(day, "- medicine");
   } else {
+    //Otherwise its rest day
     console.log(day, "- rest");
   }
 }
@@ -31,13 +36,17 @@ for (var day = 1; day <= lastDay; day++) {
 // Problem 04 - Delete / Store
 var fileName = "slipdf.txt";
 
+//getting the extension from the file
 var extension = fileName.split(".").pop();
 
 if (fileName[0] === "#") {
+  //checking if the first character is # then we are storing the value
   console.log("Store");
 } else if (extension === "pdf" || extension === "docx") {
+  //checking is the file extension is pdf and docx therefore we are storing the value of that
   console.log("Store");
 } else {
+  //If not theredfore we are deleting the value
   console.log("Delete");
 }
 
@@ -50,6 +59,7 @@ var student = {
 
 var phEmail =
   student.name + student.roll + "." + student.department + "@ph.ac.bd";
+//concating string and adding if needed
 console.log(phEmail);
 
 // Problem 06 :  Current Salary ( Challenge Problem )
@@ -57,9 +67,9 @@ var experience = 40;
 var startingSalary = 30000;
 
 for (var i = 1; i <= experience; i++) {
-  var salaryIncrementPerYear = startingSalary * (5 / 100);
-  var presentSalary = salaryIncrementPerYear + startingSalary;
-  startingSalary = presentSalary;
+  var salaryIncrementPerYear = startingSalary * (5 / 100); //Getting the salary increment
+  var presentSalary = salaryIncrementPerYear + startingSalary; //updating as present salaryu
+  startingSalary = presentSalary; //updating starting salary after the iteration
 }
 
 var salaryFloat = parseFloat(startingSalary.toFixed(2));
